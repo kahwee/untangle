b16b8056b6fbb032348a3c320d6a5d3b833beed2
KahWee Teng <t@kw.sg>
2016-01-12 16:12:19 -0800

    Add decode for quotes

--- js/app.js
+++ js/app.js
@@ -7,3 +7,19 @@ document.getElementById('encodeURIComponent').addEventListener('click', function
 document.getElementById('decodeURIComponent').addEventListener('click', function() {
 	snippet.value = decodeURIComponent(snippet.value)
 })
+
+document.getElementById('encodeFor2Quotes').addEventListener('click', function() {
+	try {
+		snippet.value = JSON.stringify([snippet.value]).slice(2).slice(0, -2)
+	} catch (e) {
+		window.alert(e.message())
+	}
+})
+
+document.getElementById('decodeFor2Quotes').addEventListener('click', function() {
+	try {
+		snippet.value = JSON.parse('["' + snippet.value + '"]')[0]
+	} catch (e) {
+		window.alert(e.message())
+	}
+})
